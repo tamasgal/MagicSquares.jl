cd569cefd642040c69b4e552c362a26029cde0e8
Tamas Gal <himself@tamasgal.com>
2017-05-08 00:25:15 +0200

    First prototype

--- src/MagicSquares.jl
+++ src/MagicSquares.jl
@@ -1,8 +1,32 @@
 module MagicSquares
 
-immutable MagicSquare{T, N}
-#    data = Array{T, 2}(N, N)
-    data = zeros(T, N, N)
+immutable MagicSquare{T<:Integer}
+    data::Array{T}
 end
+
+
+Base.shuffle!(m::MagicSquare) = shuffle!(m.data)
+
+
+function ismagical(m::MagicSquare)
+    n = convert(Int, √length(m.data))
+    s = sum(m.data[1:n])
+    data1 = reshape(m.data, n, n)
+    data2 = data1'
+    diagsum1 = s
+    diagsum2 = s
+    for i ∈ 1:n
+        if(s != sum(data1[i, :]) || s != sum(data2[i, :]))
+            return false
+        end
+        diagsum1 -= data1[i, i]
+        diagsum2 -= data1[i, n-i+1]
+    end
+    if diagsum1 == 0 && diagsum2 == 0
+        return true
+    end
+    return false
+end
+
 
 end # module
