1be267f0900910e4ea44fabbce947298313412b8
Tamas Gal <himself@tamasgal.com>
2017-05-08 15:35:42 +0200

    Working prototype

--- src/MagicSquares.jl
+++ src/MagicSquares.jl
@@ -3,15 +3,34 @@ module MagicSquares
 export MagicSquare, ismagical
 
 immutable MagicSquare{T<:Integer}
+    n::Integer
     data::Array{T}
+
+    MagicSquare(data::Array{T}) = begin
+        n = convert(Int, √length(data))
+        new(n, data)
+    end
 end
+
+MagicSquare{T <: Integer}(data::Array{T}) = MagicSquare{T}(data)
+MagicSquare{T <: Integer}(n::T) = MagicSquare{T}(zeros(T, n*n))
 
 
 Base.shuffle!(m::MagicSquare) = shuffle!(m.data)
+Base.show(io::IO, m::MagicSquare) = begin
+    if(ismagical(m))
+        println("Magic square with sum=$(sum(m.data[1:m.n]))")
+    else
+        println("Non-magic square.")
+    arr = reshape(m.data, m.n, m.n)
+    for i ∈ 1:m.n
+        println(io, arr[i, :])
+    end
+end
 
 
 function ismagical(m::MagicSquare)
-    n = convert(Int, √length(m.data))
+    n = m.n
     s = sum(m.data[1:n])
     data1 = reshape(m.data, n, n)
     data2 = data1'
